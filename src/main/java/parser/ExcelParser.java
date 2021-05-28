b4a8a5519f359fdb219816151dd87f3abaf74d80
cwong2015 <sirhc8002@gmail.com>
2021-05-28 18:36:41 -0400

    Refactored to main Driver and ExcelParser class to parse out first sheet into list of data

---
+++ src/main/java/parser/ExcelParser.java
@@ -0,0 +1,56 @@
+package parser;
+
+import org.apache.poi.xssf.usermodel.*;
+
+import java.io.*;
+import java.util.*;
+
+public class ExcelParser {
+    private File excelFile;
+    private FileInputStream fis = null;
+    private ArrayList<ArrayList<Double>> dataSet = new ArrayList<>();
+
+    public ExcelParser(String file) {
+        this.excelFile = new File(file);
+    }
+
+    public ArrayList<ArrayList<Double>> parse() {
+        try {
+            fis = new FileInputStream(excelFile);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        XSSFWorkbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook(fis);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        /* Example sheet : data starts at index 2
+            |    BTC     |
+            | Trade Date |   Size   |   Quantity   |   Price   | ... |
+            | 2021-18-04 | 0.00020..|   56,295.24  |   11.738  | ... |
+            | 2021-18-04 | 0.00028..|   55,792.72  |   15.748  | ... |
+            | 2021-19-04 | 0.00270..|   55,120.51  |   148.95  | ... |
+         */
+        int rowIndex = 2;
+        XSSFRow row = sheet.getRow(rowIndex);
+        while(row != null){
+            ArrayList<Double> innerDataSet = new ArrayList<>();
+            innerDataSet.add(row.getCell(1).getNumericCellValue());
+            innerDataSet.add(row.getCell(2).getNumericCellValue());
+            innerDataSet.add(row.getCell(3).getNumericCellValue());
+            dataSet.add(innerDataSet);
+
+            rowIndex++;
+            row = sheet.getRow(rowIndex);
+        }
+        return dataSet;
+    }
+}
