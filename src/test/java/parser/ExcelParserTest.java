a260c54cd7560572dfa9585d21f539fa7d27c548
cwong2015 <sirhc8002@gmail.com>
2021-05-31 23:26:52 -0400

    Changed ExcelParser logic from using ArrayList to HashMap to associate coin name to coin transaction data

--- src/test/java/parser/ExcelParserTest.java
+++ src/test/java/parser/ExcelParserTest.java
@@ -6,19 +6,19 @@ import java.util.*;
 
 public class ExcelParserTest {
     ExcelParser excelParser = new ExcelParser("src\\main\\resources\\Crypto Tracker.xlsx");
-    ArrayList<ArrayList<ArrayList<Double>>> sheetList = excelParser.parse();
+    HashMap<String, ArrayList<ArrayList<Double>>> sheetMap = excelParser.parse();
 
     @Test
     public void shouldEqual4Sheets() {
-        Assertions.assertEquals(sheetList.size(), 4);
+        Assertions.assertEquals(sheetMap.size(), 4);
     }
 
     @Test
     public void shouldEqualLengths() {
-        Assertions.assertEquals(sheetList.get(0).size(), 16);
-        Assertions.assertEquals(sheetList.get(1).size(), 8);
-        Assertions.assertEquals(sheetList.get(2).size(), 7);
-        Assertions.assertEquals(sheetList.get(3).size(), 7);
+        Assertions.assertEquals(sheetMap.get("BTC").size(), 16);
+        Assertions.assertEquals(sheetMap.get("ETH").size(), 7);
+        Assertions.assertEquals(sheetMap.get("MKR").size(), 8);
+        Assertions.assertEquals(sheetMap.get("ADA").size(), 7);
     }
 
 }
